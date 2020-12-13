6065a8feb13aeaf3fd4c14553994a420bf9a3b73
Edwin Troya <edtroyajr@gmail.com>
2020-12-13 00:24:01 -0800

    whats going on

--- Java/main.js
+++ Java/main.js
@@ -1,52 +1,57 @@
-
-$(document).ready(function(){
-    var contianer = $(".container");
-//   adding the time and date.
-    $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
- 
-    console.log(
-            new Date().toLocaleDateString('en-us', {
-                Weekday: "long",
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',   
-        })
-    );
-
-    console.log(new Date().getHours());
-
-    function formatTime(i) {
-    if (i == 12 ) {
-     return '12PM';
+var timeZone = moment().format('LLLL');  
+$("#currentDay").text(timeZone);
+console.log(timeZone);
+var timing = moment().format('H')
+console.log(timing);
+function structure()
+{
+    for (var i = 9; i < 18; i++) 
+    {
+        var divRow = $("<div>", { "class": "row", "id": (i) });
+        var divTime = $("<div>", { "class": "col-1 time-block hour" });
+        var divDescriptions = $("<textarea>", { "class": "col-10 description", "value": "" });
+        var divSaveBtn = $("<button>", { "class": "col-1 saveBtn" });
+    //Connecting variables
+        timeloop(divRow.attr("id"),divDescriptions);
+        divTime.text(i +"00");
+        if(localStorage.getItem(divTime.text()) !== null)
+        {
+            divDescriptions.text(localStorage.getItem(divTime.text()));
+        }
+        else{
+            divDescriptions.text("");
+        }
+        //divTime.text(i + "00");
+        divRow.append(divTime);
+        divRow.append(divDescriptions);
+        divRow.append(divSaveBtn.addClass("fas fa-save"));
+        $(".container").append(divRow);
     }
-    if (i > 12) {
-        return `${i - 12}PM`;
-     }
-
-    return `${i}AM`;
-
+}
+function timeloop (timeHour, description) {
+    if (JSON.parse(timing) > JSON.parse(timeHour)) {
+        description.addClass("past");
     }
+    else if (JSON.parse(timing) === JSON.parse(timeHour)) {
+        description.addClass("present");
+    }
+    else {
+        description.addClass("future");
+    }
+}
+
+structure();
+
+$(".saveBtn").on("click", function(){
+    var userTime = $(this).siblings(".hour").text();
+    var userText = $(this).siblings(".description").val();
+
+    localStorage.setItem(userTime,userText);
     
-    for (let i = 9; i < 18; i++) {
-
-     const timeBlock = $("<div>")
-     .attr("id", `hour-${i}`)
-     .addClass("row time-block past");
-     var tableRow
+})
      
     
-    
-  
-     timeBlock.append($('<div>').addClass('col-md-1 hour').text(formatTime(i)));
-     timeBlock.append($('<textarea>').addClass('col-md-10 description'));
-     timeBlock.append(
-         $('<button>')
-         .addClass('btn saveBtn col-md-1')
-         .append($('<i>')).addClass("fas fa-save")
 
 
-     );
-    
-     contianer.append(timeBlock)
     
 
